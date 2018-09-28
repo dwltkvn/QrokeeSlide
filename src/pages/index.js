b99d78e71a0cc710bf04f8f9a5be0abda4bf5be1
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-09-28 15:24:32 +0200

    add material-ui and hammer
    + test gatsby link navigation forwarding states

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
+import { navigate } from "gatsby";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
+import Button from "@material-ui/core/Button";
 
 const IndexPage = () => (
   <Layout>
@@ -9,7 +11,20 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={() =>
+        navigate("/page-2/", { state: { pleasant: "reasonably2" } })
+      }
+      role="link"
+      state={{
+        pleasant: "reasonably"
+      }}
+    >
+      Button
+    </Button>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 
-const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = props => {
+  console.log(props);
+  //console.log(props.location.state.pleasant);
+  const varvar = props.location.state.pleasant;
+  return (
+    <Layout>
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <p>{varvar}</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
 
-export default SecondPage
+export default SecondPage;
