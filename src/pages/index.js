5f9f9eefa404c6982457c8581dad6a04a780c50d
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-09-28 16:39:37 +0200

    begin reintegration of qrokee implem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,124 @@
 import React from "react";
-import { Link } from "gatsby";
 import { navigate } from "gatsby";
 
 import Layout from "../components/layout";
 import Button from "@material-ui/core/Button";
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Button
-      variant="contained"
-      color="primary"
-      onClick={() =>
-        navigate("/page-2/", { state: { pleasant: "reasonably2" } })
-      }
-      role="link"
-      state={{
-        pleasant: "reasonably"
-      }}
-    >
-      Button
-    </Button>
-  </Layout>
-);
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.handleEvent = this.handleEvent.bind(this);
+    this.onFilesSelected = this.onFilesSelected.bind(this);
+    this.onFileLoaded = this.onFileLoaded.bind(this);
+    this.displaySelectedImage = this.displaySelectedImage.bind(this);
+
+    this.state = {
+      stateImageLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    //window.addEventListener("event", this.handleEvent);
+  }
+
+  componentWillUnmount() {
+    //window.removeEventListener("event", this.handleEvent);
+  }
+
+  displaySelectedImage() {
+    const img = new Image();
+    img.onload = () => {
+      const nbSlideH = 2; // hardcoded, for now we just want 2 rows
+      const cubeside = img.height / nbSlideH;
+      const w = cubeside * Math.ceil(img.width / cubeside);
+      const nbSlideW = w / cubeside;
+
+      //this.props.cbStoreImgSize(w, img.height);
+      //this.props.cbStoreNbSlide(nbSlideW, nbSlideH);
+      this.storeImgSize = { w: w, h: img.height };
+      this.storeNbSlide = { w: nbSlideW, h: nbSlideH };
+
+      this.imageRef.width = w / 10;
+      this.imageRef.height = img.height / 10;
+      //console.log(img.width);
+      //console.log(img.height);
+
+      this.setState({ stateImageLoaded: true });
+    };
+    img.src = this.imgData;
+
+    this.imageRef.src = this.imgData;
+    //this.props.cbStoreImgData(this.imgData);
+    this.storeImgData = this.imgData;
+  }
+
+  onFileLoaded(e) {
+    this.imgData = e.target.result;
+    this.displaySelectedImage();
+  }
+
+  onFilesSelected(e) {
+    console.log(this.inputRef.files);
+
+    Array.from(this.inputRef.files).forEach(file => {
+      console.log(file.name);
+    });
+
+    var reader = new FileReader();
+    reader.onload = this.onFileLoaded;
+    reader.readAsDataURL(this.inputRef.files[0]);
+  }
+
+  render() {
+    return (
+      <Layout>
+        <input
+          id="uploadInput"
+          type="file"
+          name="myFiles"
+          onChange={() => this.onFilesSelected()}
+          multiple
+          ref={elem => (this.inputRef = elem)}
+          style={{ display: "none" }}
+        />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.inputRef.click()}
+        >
+          Select Image
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          role="link"
+          onClick={() =>
+            navigate("/page-2/", {
+              state: {
+                size: this.storeImgSize,
+                slide: this.storeNbSlide,
+                data: this.storeImgData
+              }
+            })
+          }
+        >
+          Continue
+        </Button>
+        <div>
+          <img
+            width="0"
+            height="0"
+            src=""
+            alt=""
+            ref={elem => (this.imageRef = elem)}
+          />
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,13 +6,18 @@ import Layout from "../components/layout";
 const SecondPage = props => {
   console.log(props);
   //console.log(props.location.state.pleasant);
-  const varvar = props.location.state.pleasant;
+  //const varvar = props.location.state.size.w;
   return (
     <Layout>
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
-      <p>{varvar}</p>
       <Link to="/">Go back to the homepage</Link>
+      <img
+        width={props.location.state.size.w}
+        height={props.location.state.size.h}
+        src={props.location.state.data}
+        alt=""
+      />
     </Layout>
   );
 };
