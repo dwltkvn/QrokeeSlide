cd6bba6d53ab12ab288fb1a8a6d0bcf99fc44364
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-10-02 22:06:13 +0200

    try to resize image to nearest pow2

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ class IndexPage extends React.Component {
       this.imageRef.height = img.height / 10;
 
       this.storeImgOrgSize = { w: img.width, h: img.height };
-
+      //console.log(this.storeImgOrgSize);
       this.setState({
         stateImageLoaded: true
       });
@@ -123,6 +123,21 @@ class IndexPage extends React.Component {
         ]);
         image.normalize();*/
         //image.resize(512, 512);
+        //const pow2H = Math.log()
+        //console.log(image.bitmap.width);
+        const h = image.bitmap.height;
+        const pow2H = Math.round(Math.log2(h));
+        const newH = Math.pow(2, pow2H);
+
+        const w = image.bitmap.width;
+        const pow2W = Math.floor(Math.log2(w));
+        const newW = Math.pow(2, pow2W);
+
+        //image.crop(0, newW, 0, newH);
+
+        //image.resize(Jimp.AUTO, newH);
+        image.resize(newW, newH);
+
         image.getBase64(Jimp.AUTO, (err, data) => {
           this.imgData = data;
           this.displaySelectedImage();
