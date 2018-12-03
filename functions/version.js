6b7cb1b0ce0a3b5a3bfb68a92402c3a67ed172eb
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-12-03 20:15:43 +0100

    correction of version lambda

--- functions/version.js
+++ functions/version.js
@@ -1,8 +1,8 @@
 const lambda = async (event, context) => {
   //console.log("H");
-  return { major: 1, minor:0, patch:21, revision:0 };
+  return { statusCode: 200, body: { major: 1, minor:0, patch:21, revision:0 } };
 };
 
-//lambda().then(data => console.log(data));
+lambda().then(data => console.log(data));
 
 exports.handler = lambda;
