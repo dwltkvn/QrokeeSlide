8c59aad7b0a0cbed79634aebc424970e7a6ae368
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-12-04 19:50:23 +0100

    inc version

--- functions/version.js
+++ functions/version.js
@@ -1,6 +1,6 @@
 const lambda = async (event, context) => {
   //console.log("H");
-  return { statusCode: 200, body: "1.0.29" };
+  return { statusCode: 200, body: "1.0.30" };
 };
 
 //lambda().then(data => console.log(data));
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "Qrokee Slider",
-    version: "1.0.29"
+    version: "1.0.30"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
