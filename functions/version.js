68174f53903e8e972ed7d447689527c90bbb8510
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-12-04 15:57:59 +0100

    inc version to test updatable detection

--- functions/version.js
+++ functions/version.js
@@ -1,6 +1,6 @@
 const lambda = async (event, context) => {
   //console.log("H");
-  return { statusCode: 200, body: "1.0.27" };
+  return { statusCode: 200, body: "1.0.28" };
 };
 
 //lambda().then(data => console.log(data));
